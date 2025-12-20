27008cbe3c2757a74747f62ae9d0b98bc29f38e9
taptrustbk-cyber <taptrust.bk@gmail.com>
2025-12-20 23:39:00 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+function sendMoney() {
+  alert("Send money feature coming next step 🚀");
+}
+
+function openMarket() {
+  alert("Market wallet opening 🛒");
+}
+
+function openWallet() {
+  alert("Wallet overview 💼");
+}
